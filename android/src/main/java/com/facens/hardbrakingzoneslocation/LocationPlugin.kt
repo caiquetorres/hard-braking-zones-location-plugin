ca94da17f99df6f4a3b68acdfbb2b19233b5f457
caiquetorres <caiquetorres.bastos@hotmail.com>
2022-01-16 10:19:01 -0300

    feat(LocationPlugin.kt): implemented the "init" method

--- android/src/main/java/com/facens/hardbrakingzoneslocation/LocationPlugin.kt
+++ android/src/main/java/com/facens/hardbrakingzoneslocation/LocationPlugin.kt
@@ -5,59 +5,106 @@ import com.getcapacitor.*
 import com.getcapacitor.annotation.CapacitorPlugin
 import com.getcapacitor.annotation.Permission
 import com.getcapacitor.annotation.PermissionCallback
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 @CapacitorPlugin(
-    name = "Location",
-    permissions = [
-        Permission(
-            alias = LocationPlugin.LOCATION,
-            strings = [
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ],
-        )
-    ]
+        name = "Location",
+        permissions = [
+            Permission(
+                    alias = LocationPlugin.LOCATION,
+                    strings = [
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                    ],
+            ),
+            Permission(
+                    alias = LocationPlugin.BACKGROUND_LOCATION,
+                    strings = [
+                        Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    ]
+            )
+        ]
 )
 class LocationPlugin : Plugin() {
     companion object {
         const val LOCATION = "location"
+        const val BACKGROUND_LOCATION = "background_location"
     }
 
     private val implementation = Location()
 
+    private fun execute() {
+        setInterval(1000) {
+            implementation.getLocation(context) { location ->
+                val response = locationToJSObject(location)
+                notifyListeners("location", response, true)
+            }
+        }
+    }
+
     @PluginMethod
-    fun getLocation(call: PluginCall?) {
-        if (call == null) {
-            throw IllegalArgumentException("plugin call expected")
+    fun init(call: PluginCall) {
+        if (getPermissionState(LOCATION) == PermissionState.GRANTED &&
+                getPermissionState(BACKGROUND_LOCATION) == PermissionState.GRANTED) {
+            execute()
+            return
         }
 
         if (getPermissionState(LOCATION) != PermissionState.GRANTED) {
             requestPermissionForAlias(LOCATION, call, "locationPermissionCallback")
-        } else {
-            implementation.getLocation(context) { location ->
-                resolve(call, location)
-            }
+            return
+        }
+
+        if (getPermissionState(BACKGROUND_LOCATION) != PermissionState.GRANTED) {
+            requestPermissionForAlias(BACKGROUND_LOCATION, call, "backgroundLocationPermissionCallback")
+            return
         }
     }
 
     @PermissionCallback
     fun locationPermissionCallback(call: PluginCall) {
         if (getPermissionState(LOCATION) == PermissionState.GRANTED) {
-            implementation.getLocation(context) { location ->
-                resolve(call, location)
+            if (getPermissionState(BACKGROUND_LOCATION) != PermissionState.GRANTED) {
+                requestPermissionForAlias(BACKGROUND_LOCATION, call, "backgroundLocationPermissionCallback")
+                return
             }
-        } else {
-            call.reject("Permission is required to get the location")
+            execute()
+            return
+        }
+
+        call.reject("Permission is required to get the location")
+    }
+
+    @PermissionCallback
+    fun backgroundLocationPermissionCallback(call: PluginCall) {
+        if (getPermissionState(BACKGROUND_LOCATION) == PermissionState.GRANTED) {
+            execute()
+            return
+        }
+
+        call.reject("Permission is required to get the background location")
+    }
+
+    private fun setInterval(milliseconds: Long, handler: () -> Unit) {
+        runBlocking {
+            launch {
+                while (true) {
+                    delay(milliseconds)
+                    handler()
+                }
+            }
         }
     }
 
-    private fun resolve(call: PluginCall, locationData: LocationData) {
+    private fun locationToJSObject(locationData: LocationData): JSObject {
         val response = JSObject()
         response.put("deviceId", locationData.deviceId)
         response.put("accuracy", locationData.accuracy)
         response.put("longitude", locationData.longitude)
         response.put("latitude", locationData.latitude)
         response.put("speed", locationData.speed)
-        call.resolve(response)
+        return response
     }
 }
